db0913adc23dd48c02ab54629414902901d9dce5
segen <segenpro@gmail.com>
2018-10-27 15:59:06 +0400

    refactoring

--- app/src/main/java/com/gmail/segenpro/simpleweather/presentation/forecast/ForecastPresenter.kt
+++ app/src/main/java/com/gmail/segenpro/simpleweather/presentation/forecast/ForecastPresenter.kt
@@ -2,7 +2,6 @@ package com.gmail.segenpro.simpleweather.presentation.forecast
 
 import android.content.Context
 import com.arellomobile.mvp.InjectViewState
-import com.gmail.segenpro.simpleweather.asErrorResult
 import com.gmail.segenpro.simpleweather.data.network.Result
 import com.gmail.segenpro.simpleweather.domain.AppSection
 import com.gmail.segenpro.simpleweather.domain.core.models.Forecast
@@ -11,9 +10,10 @@ import com.gmail.segenpro.simpleweather.domain.main.AppSectionInteractor
 import com.gmail.segenpro.simpleweather.domain.main.ReloadContentInteractor
 import com.gmail.segenpro.simpleweather.domain.weather.WeatherInteractor
 import com.gmail.segenpro.simpleweather.presentation.core.basecontentfragment.BaseContentPresenter
-import com.gmail.segenpro.simpleweather.showError
 import io.reactivex.Observable
 import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.Disposable
+import io.reactivex.disposables.Disposables
 import io.reactivex.rxkotlin.withLatestFrom
 import io.reactivex.schedulers.Schedulers
 import java.util.concurrent.TimeUnit
@@ -29,69 +29,69 @@ class ForecastPresenter @Inject constructor(context: Context,
                                             private val weatherInteractor: WeatherInteractor) :
         BaseContentPresenter<ForecastView>(context, appSectionInteractor, reloadContentInteractor) {
 
+    private var forecastTimerDisposable: Disposable = Disposables.empty()
+    private var forecastDisposable: Disposable = Disposables.empty()
+
     override fun onFirstViewAttach() {
-        getForecast()
+        getLocation()
     }
 
-    private fun getForecast() {
+    private fun getLocation() {
+        showProgress(true)
+        hideError()
         locationInteractor.getCurrentLocation()
+                .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
-                .doOnNext {
-                    hideError()
-                    showProgress(true)
-                }
-                .flatMap {
+                .doOnError { showProgress(false) }
+                .subscribe({
                     when (it) {
-                        is Result.Success -> getForecastPeriodically(it.data.name)
-                        is Result.Error -> Observable.just(it.weatherException.asErrorResult<Forecast>().showError())
+                        is Result.Success -> startForecastTimer(it.data.name)
+                        is Result.Error -> onError(it.weatherException, true)
                     }
-                }
-                .observeOn(AndroidSchedulers.mainThread())
-                .doOnEach { showProgress(false) }
-                .subscribe({ onGetForecastResult(it) }, { onError(it) })
+                }, { onError(it) })
                 .unsubscribeOnDestroy()
     }
 
-    private fun getForecastPeriodically(locationName: String): Observable<Result<Forecast>> =
-            updateRequest()
-                    .observeOn(Schedulers.io())
-                    .flatMapSingle { value ->
-                        weatherInteractor.getForecast(locationName)
-                                .map { result ->
-                                    if (value == 0L && result is Result.Error) {
-                                        result.showError()
-                                    } else {
-                                        result
-                                    }
-                                }
-                    }
+    private fun startForecastTimer(locationName: String) {
+        forecastTimerDisposable.dispose()
 
-    private fun updateRequest(): Observable<Long> = Observable.merge(
-            Observable.interval(0, FORECAST_INTERVAL_IN_MINUTES, TimeUnit.MINUTES),
-            observeReloadContentRequest()
-                    .observeOn(AndroidSchedulers.mainThread())
-                    .doOnNext {
-                        hideError()
-                        showProgress(true)
-                    }
-    )
+        forecastTimerDisposable = Observable.merge(
+                Observable.interval(0, FORECAST_INTERVAL_IN_MINUTES, TimeUnit.MINUTES),
+                observeReloadContentRequest())
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .doOnError { showProgress(false) }
+                .subscribe({
+                    getForecast(locationName, it == 0L)
+                }, { onError(it) })
+    }
 
     private fun observeReloadContentRequest(): Observable<Long> =
             reloadContentInteractor.observeReloadContentRequest()
-                    .withLatestFrom(appSectionInteractor.observeAppSection()
-                    ) { reloadedSection: AppSection, currentSection: AppSection ->
+                    .withLatestFrom(appSectionInteractor.observeAppSection())
+                    { reloadedSection: AppSection, currentSection: AppSection ->
                         reloadedSection == currentSection
                     }
                     .filter { it }
                     .map { -1L }
 
-    private fun onGetForecastResult(result: Result<Forecast>) {
+    private fun getForecast(locationName: String, isFirstResult: Boolean) {
+        forecastDisposable.dispose()
+
+        forecastDisposable = weatherInteractor.getForecast(locationName)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .doOnEvent { _, _ -> showProgress(false) }
+                .subscribe({ onGetForecastResult(it, isFirstResult) }, { onError(it) })
+    }
+
+    private fun onGetForecastResult(result: Result<Forecast>, isFirstResult: Boolean) {
         when (result) {
             is Result.Success -> {
                 hideError()
                 viewState.updateState(result.data)
             }
-            is Result.Error -> onError(result.weatherException, result.isShown)
+            is Result.Error -> onError(result.weatherException, isFirstResult)
         }
     }
 }
